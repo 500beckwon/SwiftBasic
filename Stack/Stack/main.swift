96abffe89407b25883730c2ec7c5ca116f9c5286
500beckwon <kjsx60@gmail.com>
2022-11-18 18:03:11 +0900

    Stack 내용 보강

--- Stack/Stack/main.swift
+++ Stack/Stack/main.swift
@@ -7,14 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
 
 var intStack1 = Stack<Int>(elements: [1,2,3,4,5,6,7])
 var intStack2 = Stack<Int>(elements: [])
 
-print(intStack1.isEmpty)
-print(intStack2.isEmpty)
+print("intStack1.isEmpty = \(intStack1.isEmpty)")
+print("intStack2.isEmpty = \(intStack2.isEmpty)")
 
-let pop1 = intStack1.pop()
-let pop2 = intStack2.pop()
-print(pop1, pop2)
+intStack2.pop()
+print("intStack2이 비어있는 상태에서 pop을 호출하여도 에러를 발생시키지 않는다")
+
+print("""
+      push 전 갯수 = \(intStack1.count)
+      push 전 top = \(intStack1.top())
+      """)
+
+intStack1.push(1)
+print("""
+      push 후 갯수 = \(intStack1.count)
+      push 후 top = \(intStack1.top())
+      """)
+
+
+intStack1.pop()
+print("""
+      pop 후 top = \(intStack1.top())
+      """)
+
+
