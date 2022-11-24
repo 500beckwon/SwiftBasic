650b16e1aa2910902796f22c5e5caa1d2fe1cc4f
500beckwon <kjsx60@gmail.com>
2022-11-24 16:58:19 +0900

    R.swift 기초 학습

---
+++ RSwift/RSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSwift
+//
+//  Created by ByungHoon Ann on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = R.color.purple()
+        
+        //imageView.image = R.image.smallbanner()
+        
+    }
+
+
+}
+
