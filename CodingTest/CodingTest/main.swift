76dad4cb34591fe2f55f54735f5eac8afd080940
500beckwon <kjsx60@gmail.com>
2022-12-19 20:58:30 +0900

    CodingTest 문제풀이 기록 프로젝트

---
+++ CodingTest/CodingTest/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  CodingTest
+//
+//  Created by ByungHoon Ann on 2022/12/19.
+//
+
+import Foundation
+
+
+print(pratice01("12321", "42531"))
+print(pratice01("32948209385203580", "1254522221000"))
+print(pratice01("55445", "1254522221"))
+
+print(pratice01("1000000000000", "1000"))
+print(pratice01("1000000002", "20200"))
