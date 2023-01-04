75a1ab2e539cf5668f541602f3df0209f02d6268
500beckwon <kjsx60@gmail.com>
2023-01-04 19:36:14 +0900

    11번 문제 풀이

--- CodingTest/CodingTest/Pratice11.swift
+++ CodingTest/CodingTest/Pratice11.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+
+func pratice11(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    var answer = [Int]()
+    for i in 0..<commands.count {
+        let start = commands[i][0] - 1
+        let end = commands[i][1] - 1
+        let indexK = commands[i][2] - 1
+        
+        let slice = array[start...end].sorted(by: <)
+        answer.append(slice[indexK])
+    }
+   
+    return answer
+}
+
+/*
+ 깔끔한 다른 천재의 답
+ return commands.map({(key) in
+       return array[(key[0]-1)...(key[1]-1)].sorted()[key[2]-1]
+   })
+ */
