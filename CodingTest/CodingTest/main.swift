e5cb7926c82f44707a65f23761b9f09285ff1f12
500beckwon <kjsx60@gmail.com>
2023-01-16 19:46:55 +0900

    프로그래머스 문제 풀이

--- CodingTest/CodingTest/main.swift
+++ CodingTest/CodingTest/main.swift
@@ -63,9 +63,15 @@ print(pratice03("10203", "15"))
  print(pratice12("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", 4))
 
  
-*/
- 
 var list1 = [0, 1, 2, 2, 3, 0, 4, 2]
 var list2 = [3,2,2,3]
 print(removeElement( &list1, 2))
 print(removeElement( &list2, 3))
+*/
+
+
+//print(pratice13("2022.05.19", ["A 6", "B 12", "C 3"],  ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
+
+//print(pratice13( "2020.01.01", ["Z 3", "D 5"], ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"] ))
+print(pratice13( "2020.11.28",  ["A 12"] , ["2019.12.1 A"]))
+print(pratice13("2022.02.28",  ["A 23"], ["2020.01.28 A"] ))
