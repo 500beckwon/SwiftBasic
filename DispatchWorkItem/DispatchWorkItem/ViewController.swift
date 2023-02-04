cb2b52dd2b576d4561ebf866fc5597bd2c9ee1bd
500beckwon <kjsx60@gmail.com>
2023-02-05 00:33:41 +0900

    좀더 이해하기 쉽도록 보강할 것

--- DispatchWorkItem/DispatchWorkItem/ViewController.swift
+++ DispatchWorkItem/DispatchWorkItem/ViewController.swift
@@ -11,54 +11,62 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let myGroup = DispatchGroup()
-        let serialQueue = DispatchQueue(label: "serialQueue")
-        let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
-//        let workItem = DispatchWorkItem {
-//            print(Thread.current, "workItem")
-//            for i in 1...10 {
-//                print("?", i)
-//            }
-//        }
-        
-        serialQueue.async(group: myGroup) {
-            print(Thread.current, "serialQueue")
-            for i in 1...10 {
-                print("!", i)
-            }
-        }
-        
-        serialQueue.async(group: myGroup) {
-            print(Thread.current, "serialQueue2")
-            for i in 1...10 {
-                print("#", i)
-            }
-        }
-        
-        myGroup.notify(queue: .main) {
-            print("Done - 1")
-        }
-        
-        concurrentQueue.async(group: myGroup) {
-            print(Thread.current, "concurrentQueue")
-            for i in 1...10 {
-                print("%", i)
-            }
-        }
-        
-        myGroup.notify(queue: .main) {
-            print("Done - 2")
-        }
-        
-//        serialQueue.async(group: myGroup, execute: workItem)
-//        print("#1")
-//        workItem.wait()
-//        print("#2")
-        
-        
+        // practice2()
+        // practice3()
+        // practice4()
     }
 
+    
+    func practice1() {
 
+    }
+    
+    func practice2() {
+        let workItem = DispatchWorkItem {
+            print(Thread.current)
+            for i in 1...10 {
+                print("><", i)
+            }
+        }
+        workItem.perform()
+    }
+    
+    func practice3() {
+        let annQueue = DispatchQueue(label: "Ann", qos: .userInitiated)
+        let hoonQueue = DispatchQueue(label: "hoon", qos: .userInitiated)
+        
+        annQueue.async {
+            for i in 1...5 {
+                print("Ann \(i)")
+            }
+        }
+        
+        hoonQueue.async {
+            for i in 100...105 {
+                print("hoon \(i)")
+            }
+        }
+    }
+    
+    func practice4() {
+        let myQueue = DispatchQueue(label: "MyQueue", attributes: .concurrent)
+        
+        
+        let annItem = DispatchWorkItem(qos: .userInitiated) {
+            print(Thread.current)
+            for i in 1...5 {
+                print("Ann Item = \(i)")
+            }
+        }
+        
+        let hoonItem = DispatchWorkItem(qos: .utility) {
+            print(Thread.current)
+            for i in 100...105 {
+                print("Hoon Item = \(i)")
+            }
+        }
+        myQueue.async(execute: hoonItem)
+        myQueue.async(execute: annItem)
+    }
 }
 
