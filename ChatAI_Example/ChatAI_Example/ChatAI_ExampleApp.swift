1d70de4ccce7d6dc842204d456c494277dbdd15d
500beckwon <kjsx60@gmail.com>
2023-02-07 19:24:03 +0900

    ChatAI Swift 사용해보기
    싱기방기

---
+++ ChatAI_Example/ChatAI_Example/ChatAI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAI_ExampleApp.swift
+//  ChatAI_Example
+//
+//  Created by ByungHoon Ann on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct ChatAI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAI_Example/ChatAI_Example/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  ChatAI_Example
+//
+//  Created by ByungHoon Ann on 2023/02/07.
+//
+
+import SwiftUI
+import OpenAISwift
+
+final class ViewModel: ObservableObject {
+    init() {
+        
+    }
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: "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")
+    }
+    
+    func send(text: String, completion: @escaping(String) -> Void) {
+        client?.sendCompletion(with: text, completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+        })
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(models, id: \.self) { string in
+                Text(string)
+            }
+            
+            Spacer()
+            
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("send") {
+                    send()
+                }
+            }
+        }
+        .onAppear {
+            viewModel.setup()
+        }
+        .padding()
+    }
+    
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        models.append("Me: \(text)")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                print("반응 \(response)")
+                self.models.append("ChatGPT: " + response)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
