2b2b25574d8cbd4c3f440cb128614efde6439325
500beckwon <kjsx60@gmail.com>
2023-06-22 18:29:01 +0900

    6. Zigzag Conversion

--- CodingTest/CodingTest/LeetCode/LeetCode37.swift
+++ CodingTest/CodingTest/LeetCode/LeetCode37.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+func convert(_ s: String, _ numRows: Int) -> String {
+    let textList = s.map { String($0) }
+    var currentRow = 0
+    var dic = [Int: [String]]()
+    var upRow = true
+    
+    for text in textList {
+        dic[currentRow, default: []].append(text)
+
+        if upRow == true {
+            if currentRow == numRows - 1 {
+                currentRow -= 1
+                upRow = false
+            } else {
+                currentRow += 1
+            }
+        } else {
+            if currentRow == 0 {
+                currentRow += 1
+                upRow = true
+            } else {
+                currentRow -= 1
+            }
+        }
+    }
+    
+    var resultText = ""
+    
+    dic.sorted(by: { $0.key < $1.key }).forEach {
+        resultText += $0.value.joined()
+    }
+    
+    return resultText
+}
