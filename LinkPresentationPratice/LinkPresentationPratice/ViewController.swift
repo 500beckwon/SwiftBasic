fb698299e860aa3f34ab898e7c95479961981d3b
500beckwon <kjsx60@gmail.com>
2024-02-20 18:09:11 +0900

    URL 프리뷰 기능 테스트

---
+++ LinkPresentationPratice/LinkPresentationPratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkPresentationPratice
+//
+//  Created by dev dfcc on 2/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
